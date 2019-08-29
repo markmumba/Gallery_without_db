fa7956aa3e54ffa4fdbe7e565add6df222561233
markmumba <markmumba01@gmail.com>
2019-08-29 13:21:55 +0300

    adding the static files

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
+from django.templatetags.static import static
 
 # Create your views here.
+
+def index(request):
+    path = settings.MEDIA_ROOT
+    img_list = os.listdir(path + '/images')
+    context = {'images' : img_list}
+    return render(request, "photo/index.html", context)
+
+    
